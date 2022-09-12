a12829935807578c270c4711ec03c26a5ff35e50
ChrisKnowsDev <chrisharley81@gmail.com>
2022-09-12 12:23:00 -0400

    defined global vars for UI elements and created event listener to listen for focus in input

--- src/app.js
+++ src/app.js
@@ -5,3 +5,13 @@
 // once both players have gone the higher correct wins the round and their current wins var is updated
 // once one player has two wins the win the game and their total wins var is updated
 // the current wins var and correct var go back to zero and new game is started on focus again
+const restartBtn = document.querySelector('.restart')
+const player1 = document.querySelector('.player-1')
+const player2 = document.querySelector('.player-2')
+const round = document.querySelector('.round')
+const word = document.querySelector('.word')
+const input = document.querySelector('.input')
+
+input.addEventListener('focus', () => {
+  console.log('user just focused on the input')
+})

a1fa8369106815e4ac02a1e7f0fc5f5a0a0ae567
ChrisKnowsDev <chrisharley81@gmail.com>
2022-09-12 11:15:30 -0400

    added sudo code

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// all stats set to 0
+// once user focus on input => countdown starts
+// words appear and change as user types correctly
+// when time is up the player correct var is updated and  other player goes
+// once both players have gone the higher correct wins the round and their current wins var is updated
+// once one player has two wins the win the game and their total wins var is updated
+// the current wins var and correct var go back to zero and new game is started on focus again

d5a5d5cbe58058b738c3aeafa621d50803731077
ChrisKnowsDev <chrisharley81@gmail.com>
2022-09-15 12:48:16 -0400

    refactory code... currently user starts game and edits text as types

--- src/app.js
+++ src/app.js
@@ -1,33 +1,29 @@
-// DOM elements
+// DOM Elements
 const restartBtn = document.querySelector('.restart')
-const player1El = document.querySelector('.player-0')
-const player2El = document.querySelector('.player-1')
+const player0El = document.querySelector('.player-0')
+const player1El = document.querySelector('.player-1')
 const roundEl = document.querySelector('.round')
 const wordEl = document.querySelector('.word')
 const inputEl = document.querySelector('.input')
-const accuracyEl = document.querySelector('.accuracy')
 const countdownEl = document.querySelector('#session-info').querySelector('p')
 
 // vars to keep track of things
+let round = 1
+const playerWordCount = [0, 0]
 const currentWins = [0, 0]
 const totalWins = [0, 0]
-const correct = [0, 0]
+const correctWords = 0
 let currentPlayer = 0
-let currentScore = 0
-let timeLimit = 20
-let currentWord = ''
-let round = 1
-let timeLeft = timeLimit
-let timeElapsed = 0
-let timer = null
-let wordIndex = 0
-let letters = 0
-let words = 0
-let charTyped = 0
-let errors = 0
 let totalErrors = 0
+let errors = 0
+let wordIndex = 0
+let timeLimit = 10
+let timer
+let timeLeft = timeLimit
+let currentInputLetter
+let currentWord
 
-// array of words to type
+// array of words to be typed
 const wordsArray = [
   'Ablutophobia',
   'Arachibutyrophobia',
@@ -51,64 +47,80 @@ const wordsArray = [
   'Katathermometer',
 ]
 
-// ===== START GAME =====
-// start game when user focuses on the input box.... reset all values, update the word, create a new timer
-function startGame() {
-  // reset all values back to zero except for total wins
-  // wait on this for now
-  resetValues()
-  // run updateWord function to get a new word
+// ===== start game =====
+function startRound() {
+  reset()
   updateWord()
-
-  // clear old timer and start a new one (STARTS TIMER)
+  // clear all timers
   clearInterval(timer)
-  // run this functio nevery second which updates the timer
+  // run the function updateTimer every 1 sec => start new timers
   timer = setInterval(updateTimer, 1000)
 }
 
-// ===== UPDATE WORD =====
-// get a word from the word array and split the chars into span elements
+// ===== reset =====
+function reset() {
+  console.log('reseting')
+}
+
+// ===== update word =====
 function updateWord() {
-  // needs this or else word won't update
+  // removes the loading text once clicking into input
   wordEl.textContent = null
+
+  // get a word to type from the array of words
   currentWord = wordsArray[wordIndex]
 
-  // seperates each letter and make a span of each to style each letter individually => this will be a h1 with each letter as a child span
+  // seperates each letter of cuurent word and creates a span and appends to the current word h1
   currentWord.split('').forEach(char => {
-    const charSpan = document.createElement('span')
-    charSpan.innerText = char
-    wordEl.appendChild(charSpan)
+    const span = document.createElement('span')
+    span.innerText = char
+    wordEl.appendChild(span)
   })
 
-  // if the current word is not the last word move to the next word, if its the last word reset the index to 0 and start over
-  if (wordIndex < wordsArray.length - 1) {
-    wordIndex++
-  } else {
-    wordIndex = 0
+  // each time this function is called update the wordIndex to change the word
+  wordIndex < wordsArray.length - 1 ? wordIndex++ : (wordIndex = 0)
+}
+
+// ===== updateTimer =====
+function updateTimer() {
+  if (timeLeft > 0) {
+    timeLeft--
+    countdownEl.textContent = `${timeLeft}s Remaining`
   }
 }
 
-// ===== PROCESS WORD =====
-// get the current value of the input box, color text as typed, calculate letters typed correctly, move to next word
-function proccessCurrentWord() {
-  // store the current char typed in a var
-  curInput = inputEl.value
-  // create an arr of the typed chars
-  curInputArr = curInput.split('')
+// ===== handleChangeOver =====
+function handleChangeOver() {
+  let winner
+  const [p0WordCount, p1WordCount] = playerWordCount
+  if (p0WordCount === 2 || p1WordCount === 2) {
+    if (p0WordCount > p1WordCount) {
+      winner = 'player 1'
+      totalWins[0]++
+    } else {
+      winner = 'player 2'
+      totalWins[1]++
+    }
+    finishGame(winner)
+  }
+}
 
-  // increment the var charTyped which holds the number of chars typed
-  charTyped++
-  errors = 0
+// ===== editTypedWord =====
+// all this function does is edit the word to be typed by adding and removing css classes showing green for correctly tpyed letter and red for wrong letter typed
+function editTypedWord() {
+  currInputLetter = inputEl.value
+  const currInputArray = currInputLetter.split('')
 
-  // get all span elements from the word h1
-  wordsSpanArray = wordEl.querySelectorAll('span')
-  // loop over the array of spans to check if user input is correct or not
-  wordsSpanArray.forEach((char, index) => {
-    // get the current char in the input arr
-    let typedChar = curInputArr[index]
+  // get all spans from h1
+  wordSpanArray = wordEl.querySelectorAll('span')
+
+  // loop over array to see if user typed correct letter or not
+  wordSpanArray.forEach((char, i) => {
+    // get the current typed letter from the currInputArray
+    let typedChar = currInputArray[i]
 
     // nothing entered
-    if (typedChar == null) {
+    if (!typedChar) {
       char.classList.remove('correct')
       char.classList.remove('incorrect')
     }
@@ -119,98 +131,19 @@ function proccessCurrentWord() {
     }
     // incorrect
     else {
-      char.classList.add('incorrect')
       char.classList.remove('correct')
-      errors++
+      char.classList.add('incorrect')
     }
   })
-
-  // calculate total letters correct
-  let correctChars = charTyped - errors
-
-  // if the length of the current input word is the same as the current word to be typed then update to a new word
-  if (curInput === currentWord) {
-    updateWord()
-    inputEl.value = ''
-    words++
-  }
 }
 
-// ===== RESET VALUES =====
-// reset all values in UI back to zero except for total wins
-function resetValues(checkWordUpdate) {
-  timeLeft = timeLimit
-  timeElapsed = 0
-  errors = 0
-  totalErrors = 0
-  accuracy = 0
-  charTyped = 0
-  wordIndex = 0
-  inputEl.disabled = false
-  inputEl.value = ''
-  clearInterval(timer)
-  countdownEl.textContent = `${timeLeft}s Remaining`
-  if (checkWordUpdate) {
-    wordEl.textContent = 'loading...'
-    words = 0
-    document
-      .querySelector(`.player-${currentPlayer}`)
-      .querySelector('.words').textContent = words
-  }
+// ===== finishGame =====
+function finishGame(winner) {
+  console.log(`Game Over: ${winner} won`)
 }
 
-// ===== UPDATE TIMER =====
-// update the time, finish the game
-function updateTimer() {
-  // when update timer function is called which, setInterval is set to 1000ms (1s)
-  if (timeLeft > 0) {
-    timeLeft--
-    timeElapsed++
-    countdownEl.textContent = `${timeLeft}s Remaining`
-  }
-  // if timeLeft is not > 0 then the round is over, update the timer to round over and run finish game
-  else {
-    // at this point no time left on countdown, turn is over... check if one player has 2 wins and if so game over if not then switch players
-    if (currentWins[0] === 2 || currentWins[1] === 2) {
-      countdownEl.textContent = 'game over'
-      resetValues()
-      finishGame()
-    } else {
-      // switch player
-      document
-        .querySelector(`.player-${currentPlayer}`)
-        .querySelector('.words').textContent = 0
-      currentPlayer = currentPlayer === 0 ? 1 : 0
-      resetValues()
-      finishGame()
-      words = 0
-    }
-  }
-}
-
-// ===== FINISH GAME =====
-// finish game.. delete the timer, calculate number of words typed for each player and determine who won
-function finishGame() {
-  // stop timer
-  clearInterval(timer)
-
-  // disable the input
-  // save this for when the game is over
-  // inputEl.disabled = true
-  // update the word text
-  wordEl.textContent = 'round over...'
-
-  // restart the game when user focuses on input
-  inputEl.addEventListener('click', startGame)
-
-  // calculate amount of words typed
-  // update the words var in player info
-  document
-    .querySelector(`.player-${currentPlayer}`)
-    .querySelector('.words').textContent = `Words: ${words}`
-}
-
-// ===== EVENT LISTENERS =====
-inputEl.addEventListener('input', proccessCurrentWord)
-inputEl.addEventListener('focus', startGame)
-restartBtn.addEventListener('click', () => resetValues(true))
+// ===== event listeners =====
+// listen for when user clicks/focuses into input
+inputEl.addEventListener('focus', startRound)
+// listen for input every change
+inputEl.addEventListener('input', editTypedWord)

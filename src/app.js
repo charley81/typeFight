cd3e856cf67b2d98a62f4d19e6ce841ebbbc425b
ChrisKnowsDev <chrisharley81@gmail.com>
2022-09-16 09:06:28 -0400

    updating current player with a border below name

--- src/app.js
+++ src/app.js
@@ -105,6 +105,8 @@ function updateTimer() {
     currentPlayer = currentPlayer === 0 ? 1 : 0
     correctWords = 0
     wordEl.textContent = currentPlayer === 0 ? 'Player 1' : 'Player 2'
+    player0El.querySelector('h3').classList.toggle('current-player')
+    player1El.querySelector('h3').classList.toggle('current-player')
   }
 }
 
